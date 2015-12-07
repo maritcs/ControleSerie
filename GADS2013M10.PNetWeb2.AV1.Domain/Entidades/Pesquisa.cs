9368c6e811caf553500974a97c306ba3ce4fa915
maritcs <mariana_tcs@yahoo.com.br>
2015-12-07 01:17:15 -0200

    Av1Parte3

---
+++ GADS2013M10.PNetWeb2.AV1.Domain/Entidades/Pesquisa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADS2013M10.PNetWeb2.AV1.Domain.Entidades
+{
+    public class Pesquisa
+    {
+        public int CanalTvId { get; set; }
+    }
+}
--- GADS2013M10.PNetWeb2.AV1.Presentation.MVC/Controllers/HomeController.cs
+++ GADS2013M10.PNetWeb2.AV1.Presentation.MVC/Controllers/HomeController.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GADS2013M10.PNetWeb2.AV1.Domain.Entidades;
+using GADS2013M10.PNetWeb2.AV1.Domain.Contexto;
 
 namespace GADS2013M10.PNetWeb2.AV1.Presentation.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private INFNETDbContext db = new INFNETDbContext();
+
         public ActionResult Index()
         {
+            //ViewBag.Serie = new SelectList(db.Series, "SerieId", "NomeSerie");
+            ViewBag.CanalTv = new SelectList(db.CanaisTv, "CanalTvId", "NomeCanal");
+
             return View();
+        }
+
+        public ActionResult Pesquisar(Pesquisa pesquisa)
+        {
+            var serie = from m in db.Series
+                          where m.CanalTvId == pesquisa.CanalTvId
+                          select new ResultadoPesquisa {NomeSerie = m.NomeSerie};
+
+            return Json(serie, JsonRequestBehavior.AllowGet);
         }
     }
 }
